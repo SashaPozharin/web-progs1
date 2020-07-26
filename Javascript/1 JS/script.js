6f0d464ca61d46dd0082c507f98ce3c89dcad047
SashaPozharin <persill121@gmail.com>
2020-07-26 14:55:36 +0300

    первый коммит

---
+++ Javascript/1 JS/script.js
@@ -0,0 +1,10 @@
+let result = 0;
+let a = 245;
+let b = 421;
+result = b / a;
+
+txt.innerHTML = result;
+
+result1.innerHTML = a*2-b*2*a+result*0;
+
+sasha.innerHTML = "Саша";
---
+++ Javascript/2 JS/script.js
@@ -0,0 +1,6 @@
+function setResult(){
+    alert();
+    result.innerHTML ="Я научился понимать JS";
+}
+
+btn.addEnentListener( "click", setResult);
