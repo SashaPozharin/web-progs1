4e780c9ff66f206c32daa0118ef153add0f804ed
SashaPozharin <68804550+SashaPozharin@users.noreply.github.com>
2020-08-16 15:01:35 +0300

    Занятие 16.08.2020

---
+++ Javascript/4 JS/script.js
@@ -0,0 +1,7 @@
+let div = document.createElement("div");
+div.innerHTML = "Я создан JSом";
+div.style.height = "50px";
+div.style.width = "150px";
+div.style.backgroundColor = "blue";
+console.log(div);
+document.body.appendChild(div)
