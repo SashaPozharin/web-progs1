4970140aef15d8df4f5d8887a171d18f3664952d
SashaPozharin <68804550+SashaPozharin@users.noreply.github.com>
2020-08-30 14:45:37 +0300

    Занятие 30,08,2020

--- Javascript/5 JS/script.js
+++ Javascript/5 JS/script.js
@@ -18,7 +18,7 @@ for(let i = 0; i < colors.length; i++) {
 pallete.style.marginLeft = (window.innerWidth/2) - ((colors.length*50)/2) + "px";
 
 let canvas = document.createElement("canvas");
-canvas.id = "field";
+canvas.id = "canvas";
 canvas.style.border = "2px solid black";
 canvas.width = window.innerWidth/2;
 canvas.height = window.innerHeight*0.8;
@@ -27,7 +27,12 @@ canvas.style.marginTop = "50px";
 document.body.appendChild(canvas);
 
 
-let color = "black"
+let color = "black";
+let brushSize = 2;
+let prevX = prevY = 0;
+let currX = currY = 0;
+let isDraw = false;
+
 for(let i=0; i < colors.length; i++){
     document.getElementById(colors[i]).addEventListener("click", function(event){
         for(let i=0; i < colors.length; i++){
@@ -39,4 +44,42 @@ for(let i=0; i < colors.length; i++){
         color = event.target.id;
     })
 
+}
+
+canvas.addEventListener("mouseup", function(event){
+    isDraw = false;
+})
+canvas.addEventListener("mouseout", function(event){
+    isDraw = false;
+})
+
+canvas.addEventListener("mousedown", function(event){
+    isDraw = true;
+    prevX = currX;
+    prevY = currY;
+    currX = event.offsetX;
+    currY = event.offsetY;
+    
+})
+
+canvas.addEventListener("mousemove", function(event){
+    if(isDraw == true){
+        console.log(1)
+        prevX = currX;
+        prevY = currY;
+        currX = event.offsetX;
+        currY = event.offsetY; 
+        draw(); 
+    }
+
+})
+function draw(){
+    let context = canvas.getContext("2d");
+    context.beginPath();
+    context.moveTo(prevX, prevY);
+    context.lineTo(currX, currY);
+    context.strokeStyle = color;
+    context.lineWidth = brushSize;
+    context.stroke();
+    context.closePath();
 }
