80c259e42cd05ec642689651666eb7f0f01605a0
SashaPozharin <68804550+SashaPozharin@users.noreply.github.com>
2020-09-20 10:55:29 +0300

    Домашка на 20.09.20

--- Javascript/6 JS/script.js
+++ Javascript/6 JS/script.js
@@ -6,19 +6,26 @@ function onSubmit(event){
     let name = form.userName.value;
     let age = form.userAge.value;
     let from = form.userFrom.value;
-    // в разработке
-    // let like = form.userLike.value;
+
 
     console.log("name: " + name);
     console.log("age: " + age);
     console.log("from: " + from);
-    // в разработке
-    // console.log("like: " + like);
+
+    let userLike = document.getElementsByClassName("userLike");
+    let like = [];
+
+    for(let i = 0; i<userLike.length; i++){
+        if(userLike[i].checked == true){
+            like.push(userLike[i].value);
+        }
+    }
+    console.log(like);
 
     let sA = "Жил-был, паренек веселый, жил здорово. Звали его " + name;
     let sB = ", отроду ему было " + age;
     let sC = ". И работал он в " + from;
-    story.innerHTML = sA + sB + sC;
+    let SD = ". Любил он c ребятами " + like;
+    story.innerHTML = sA + sB + sC + SD;
 }
-
 popitka.addEventListener("submit", onSubmit);
