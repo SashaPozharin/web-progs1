fc64fea8286c9595e58e7c7681501fe58a7c379b
SashaPozharin <68804550+SashaPozharin@users.noreply.github.com>
2021-01-31 11:42:13 +0300

    Add files via upload

--- React_js/дз на 31.01/index.js
+++ React_js/дз на 31.01/index.js
@@ -1,8 +1,3 @@
 let serverData = [24, "User", false, 85, -5, 0, 45, "google.com"];
-let userInfo =[];
-let digits = [];
-let url;
-serverData.forEach((item, i) => {if (typeof item == "string"){if (item.endsWith(".com")){url = item;} else {userInfo.push(item);}}if (typeof item == "number"){digits.push(item);}});
-console.log(userInfo);
-console.log(digits);
-console.log(url+`\n`);
+let [url, digts, userInfo] = [serverData.filter((item, i)=>{return (typeof item == "string" && item == 'google.com')}), serverData.filter((item, i)=>{return typeof item == "number"}), serverData.filter((item, i)=>{return (typeof item == "string" && item !="google.com")})];
+console.log(userInfo, digts, url);
